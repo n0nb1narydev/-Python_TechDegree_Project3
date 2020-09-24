90d452fbca643ac20f3e92ee367c3c0984980f05
Mel Rumsey <melsrumsey@gmail.com>
2020-09-24 12:33:00 -0500

    imported

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-import game
-import phrase
+from game import Game
+from phrase import Phrase
 
-# Import your Game class
+
+
+
 
 # Create your Dunder Main statement.
-
+if __name__ == "__main__":
 # Inside Dunder Main:
+    game = Game()
+    phrase = Phrase()
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
--- phrase.py
+++ phrase.py
@@ -1,4 +1,5 @@
 # Create your Phrase class logic here.
 
 class Phrase:
-    phrases = ['Prune Juice: A Warriors Drink','Fascinating','Resistance is Futile',"There's Coffee in that Nebula","I'm a Doctor not an escalator",'Today is a good day to die.','There are four lights!','Never allow family to stand in the way of opportunity.',]
+    # phrases = ['Prune Juice: A Warriors Drink','Fascinating','Resistance is Futile',"There's Coffee in that Nebula","I'm a Doctor not an escalator",'Today is a good day to die.','There are four lights!','Never allow family to stand in the way of opportunity.',]
+    pass
